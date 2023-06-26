4d3ec9a91ec1f53c3d85989192022a59ceede453
leliasgomer <leliasgomer@gmail.com>
2023-06-26 09:16:56 -0500

    fix: :bug: master change color at navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -31,7 +31,7 @@ export default function NavBar() {
     <nav className='w-[1500px] bg-[#f8f1e8] dark:bg-[#141414] duration-200 flex items-center justify-between py-5 max-[1530px]:w-[95%] fixed max-[750px]:py-3 z-40'>
       <span className='font-medium text-[#db865b] text-[20px] '>Cafecito</span>
       <div className='flex items-center gap-10 max-[750px]:gap-5'>
-        <ul className='list flex gap-10 items-center max-[750px]:absolute max-[750px]:w-full max-[750px]:left-0 max-[750px]:top-[-400%] max-[750px]:flex-col max-[750px]:gap-5 max-[750px]:py-5 max-[750px]:z-20 max-[750px]:bg-[#fcf6ee] dark:max-[750px]:bg-[#361b0b] duration-200'>
+        <ul className='list flex gap-10 items-center max-[750px]:absolute max-[750px]:w-full max-[750px]:left-0 max-[750px]:top-[-400%] max-[750px]:flex-col max-[750px]:gap-5 max-[750px]:py-5 max-[750px]:z-20 max-[750px]:bg-[#f8f1e8] dark:max-[750px]:bg-[#141414] duration-200'>
           <li><a className='text-[17px]' href='#inicio'>Inicio</a></li>
           <li><a className='text-[17px]' href='#SobreMi'>Sobre Mi</a></li>
           <li><a className='text-[17px]' href='#proyectos'>Proyectos</a></li>
